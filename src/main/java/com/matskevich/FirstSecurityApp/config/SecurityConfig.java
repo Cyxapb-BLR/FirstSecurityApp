2292112cf9893e192160c6dfc616fadf9df8618b
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-08 16:00:46 -0500

    L78 -//- fix

--- src/main/java/com/matskevich/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/matskevich/FirstSecurityApp/config/SecurityConfig.java
@@ -4,10 +4,10 @@ import com.matskevich.FirstSecurityApp.security.AuthProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthProviderImpl authProvider;
 
     @Autowired
