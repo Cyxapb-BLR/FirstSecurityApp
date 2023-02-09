c0ef838bd965131468db3d66e56557e81eabf2f6
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-09 13:12:16 -0500

    L81 - Registration Process in Spring Sequrity - added registration methods in AuthController and registration.html

--- src/main/java/com/matskevich/FirstSecurityApp/controllers/AuthController.java
+++ src/main/java/com/matskevich/FirstSecurityApp/controllers/AuthController.java
@@ -1,8 +1,13 @@
 package com.matskevich.FirstSecurityApp.controllers;
 
+import com.matskevich.FirstSecurityApp.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
@@ -10,5 +15,15 @@ public class AuthController {
     @GetMapping("/login")
     public String loginPage() {
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("person") Person person) {
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("person") @Valid Person person) {
+        //TODO
     }
 }
