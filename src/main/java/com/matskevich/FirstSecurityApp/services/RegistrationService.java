4ac82acf9ae3f0bd2b788779d6ed7456430ee6a7
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-09 14:33:40 -0500

    L83 - Password Encoder

--- src/main/java/com/matskevich/FirstSecurityApp/services/RegistrationService.java
+++ src/main/java/com/matskevich/FirstSecurityApp/services/RegistrationService.java
@@ -3,20 +3,28 @@ package com.matskevich.FirstSecurityApp.services;
 import com.matskevich.FirstSecurityApp.models.Person;
 import com.matskevich.FirstSecurityApp.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+       /* String password = person.getPassword();
+        String encodedPassword = passwordEncoder.encode(password);
+        person.setPassword(encodedPassword);*/
+
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
