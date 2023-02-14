8e8c3471661f5a52bb22c3645264f3399700aef0
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-13 19:51:10 -0500

    L95 - JWT - created DTO

---
+++ src/main/java/com/matskevich/FirstSecurityApp/dto/AuthenticationDTO.java
@@ -0,0 +1,28 @@
+package com.matskevich.FirstSecurityApp.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class AuthenticationDTO {
+    @NotEmpty(message = "Username should not be empty")
+    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/matskevich/FirstSecurityApp/dto/PersonDTO.java
@@ -0,0 +1,41 @@
+package com.matskevich.FirstSecurityApp.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class PersonDTO {
+    @NotEmpty(message = "Username should not be empty")
+    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
+    private String username;
+
+    @Min(value = 1900, message = "Year should be greater than 1900")
+    private int yearOfBirth;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
